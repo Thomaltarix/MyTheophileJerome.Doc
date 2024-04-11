66590ff7e22a89beebc6c48d28793da7d9773ecd
Mathieu Robert <mathieu1.robert@epitech.eu>
2024-04-11 10:03:49 +0200

    feat: display usage when no arguments

--- src/MyPandoc.hs
+++ src/MyPandoc.hs
@@ -14,7 +14,7 @@ import System.Exit(exitWith, ExitCode(ExitFailure))
 import ParsingCommand
 
 myPandoc :: [String] -> IO ()
-myPandoc [] = exitWith(ExitFailure 84)
+myPandoc [] = displayUsage >> exitWith(ExitFailure 84)
 myPandoc args = case parseArgs args of
                           Nothing -> displayUsage >> exitWith(ExitFailure 84)
                           Just conf -> print conf
