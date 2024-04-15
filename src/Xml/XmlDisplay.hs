18be4a10c90986ed782fc9dcc50b047c47c47aaa
Thomaltarix <thomas.boue@epitech.eu>
2024-04-15 16:09:53 +0200

    feat: XML Display Module

---
+++ src/Xml/XmlDisplay.hs
@@ -0,0 +1,35 @@
+{-
+-- EPITECH PROJECT, 2024
+-- MyTh-ophileJ-r-me.Doc
+-- File description:
+-- xmlDisplay
+-}
+
+module Xml.XmlDisplay (
+    getXmlObjectTag,
+    getXmlDataTag,
+    printXml
+    ) where
+
+import System.IO
+import DataStructure
+
+getXmlObjectTag :: Object -> (String, String)
+getXmlObjectTag obj = case objType obj of
+    SectionT -> ("<section>", "</section>")
+    ListT -> ("<list>", "</list>")
+    CodeBlockT -> ("<codeblock>", "</codeblock>")
+    LinkT -> ("<link>", "</link>")
+    ParagraphT -> ("<paragraph>", "</paragraph>")
+    ImageT -> ("<image>", "</image>")
+
+getXmlDataTag :: Data -> (String, String)
+getXmlDataTag data_ = case dataType data_ of
+    TextT -> ("", "")
+    ItalicT -> ("<italic>", "</italic>")
+    BoldT -> ("<bold>", "</bold>")
+    CodeT -> ("<code>", "</code>")
+
+
+printXml :: Maybe Handle -> DataStruct -> IO ()
+printXml _ _ = putStrLn "XML"
