ed603e8966634cdf67936598a50e875aa695637e
Thomaltarix <thomas.boue@epitech.eu>
2024-04-22 14:26:29 +0200

    feat: Xml Header display

--- src/Xml/XmlDisplay.hs
+++ src/Xml/XmlDisplay.hs
@@ -30,6 +30,26 @@ getXmlDataTag data_ = case dataType data_ of
     BoldT -> ("<bold>", "</bold>")
     CodeT -> ("<code>", "</code>")
 
+printXmlHeaderData :: Maybe Handle -> Maybe Data -> Int -> IO ()
+printXmlHeaderData _ Nothing _ = return ()
+printXmlHeaderData handle (Just data_@(Data {symbol = Just "title"})) _ =
+    printString handle (myFromJustString (dataContent data_)) 0
+printXmlHeaderData handle (Just data_) spaces =
+    printXmlStartSymbol handle (myFromJustString (symbol data_)) spaces >>
+    printString handle (myFromJustString (dataContent data_)) 0 >>
+    printXmlEndSymbol handle (myFromJustString (symbol data_)) 0
+
+printXmlHeader :: Maybe Handle -> Header -> Int -> IO ()
+printXmlHeader handle header_ spaces =
+    let (startTag, endTag) = getXmlTag HeaderT in
+    printString handle startTag spaces >>
+    printString handle " title=\"" 0 >>
+    printXmlHeaderData handle (title header_) 0 >>
+    printString handle "\">\n" 0 >>
+    printXmlHeaderData handle (author header_) (spaces + 4) >>
+    printXmlHeaderData handle (date header_) (spaces + 4) >>
+    printString handle endTag spaces
 
 printXml :: Maybe Handle -> DataStruct -> IO ()
 printXml _ _ = putStrLn "XML"
+    printXmlHeader handle (header dataStruct) 4 >>
