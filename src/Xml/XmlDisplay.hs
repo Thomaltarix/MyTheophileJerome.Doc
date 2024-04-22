21f60fa2569b9864db10e3dc3046a845fe6dee04
Thomaltarix <thomas.boue@epitech.eu>
2024-04-22 14:28:56 +0200

    feat: Xml Data display

--- src/Xml/XmlDisplay.hs
+++ src/Xml/XmlDisplay.hs
@@ -13,23 +13,84 @@ module Xml.XmlDisplay (
 
 import System.IO
 import DataStructure
+import PrintString
+
+data XmlTypes = DocumentT | HeaderT | BodyT deriving Eq
+
+getXmlTag :: XmlTypes -> (String, String)
+getXmlTag DocumentT = ("<document>\n", "</document>\n")
+getXmlTag HeaderT = ("<header", "</header>\n")
+getXmlTag BodyT = ("<body>\n", "</body>\n")
 
 getXmlObjectTag :: Object -> (String, String)
 getXmlObjectTag obj = case objType obj of
-    SectionT -> ("<section>", "</section>")
-    ListT -> ("<list>", "</list>")
-    CodeBlockT -> ("<codeblock>", "</codeblock>")
-    LinkT -> ("<link>", "</link>")
-    ImageT -> ("<image>", "</image>")
-    AltT -> ("<alt>", "</alt>")
+    SectionT -> ("<section", "</section>\n")
+    ListT -> ("<list>\n", "</list>\n")
+    CodeBlockT -> ("<codeblock>\n", "</codeblock>\n")
+    LinkT -> ("<link", "</link>\n")
+    ParagraphT -> ("<paragraph", "</paragraph>\n")
+    ImageT -> ("<image", "</image>\n")
+    AltT -> ("", "")
 
 getXmlDataTag :: Data -> (String, String)
 getXmlDataTag data_ = case dataType data_ of
-    TextT -> ("", "")
+    TextT -> ("<paragraph>", "</paragraph>\n")
     ItalicT -> ("<italic>", "</italic>")
     BoldT -> ("<bold>", "</bold>")
     CodeT -> ("<code>", "</code>")
 
+printXmlStartSymbol :: Maybe Handle -> String -> Int -> IO ()
+printXmlStartSymbol handle "" spaces = printString handle "" spaces
+printXmlStartSymbol handle symbol_ spaces =
+    printString handle "<" spaces >>
+    printString handle symbol_ 0 >>
+    printString handle ">" 0
+
+printXmlEndSymbol :: Maybe Handle -> String -> Int -> IO ()
+printXmlEndSymbol handle "" spaces = printString handle "\n" spaces
+printXmlEndSymbol handle symbol_ _ =
+    printString handle "</" 0 >>
+    printString handle symbol_ 0 >>
+    printString handle ">\n" 0
+
+printXmlData :: Maybe Handle -> Data -> Int -> Bool -> IO ()
+printXmlData _ Data {symbol = Just "title"} _ _ = return ()
+printXmlData handle data_@(Data {dataType = ItalicT}) _ _ =
+    let (startTag, endTag) = getXmlDataTag data_ in
+    printString handle startTag 0 >>
+    printString handle (myFromJustString (dataContent data_)) 0 >>
+    printString handle endTag 0
+printXmlData handle data_@(Data {dataType = BoldT}) _ _ =
+    let (startTag, endTag) = getXmlDataTag data_ in
+    printString handle startTag 0 >>
+    printString handle (myFromJustString (dataContent data_)) 0 >>
+    printString handle endTag 0
+printXmlData handle data_@(Data {dataType = CodeT}) _ _ =
+    let (startTag, endTag) = getXmlDataTag data_ in
+    printString handle startTag 0 >>
+    printString handle (myFromJustString (dataContent data_)) 0 >>
+    printString handle endTag 0
+printXmlData handle data_@(Data {dataType = TextT}) spaces _ =
+    let (startTag, endTag) = getXmlDataTag data_ in
+    printString handle startTag spaces >>
+    printString handle (myFromJustString (dataContent data_)) 0 >>
+    printString handle endTag 0
+
+printXmlSpecialData :: Maybe Handle -> [Either Data Object] -> Int -> String -> IO ()
+printXmlSpecialData _ [] _ _ = return ()
+printXmlSpecialData handle [x] _ symbol_ = case x of
+    Left data_ ->   if symbol_ == myFromJustString (symbol data_) then
+                        printString handle " " 0 >>
+                        printString handle symbol_ 0 >>
+                        printString handle "=\"" 0 >>
+                        printString handle (myFromJustString (dataContent data_)) 0 >>
+                        printString handle "\">\n" 0
+                    else return ()
+    Right _ -> return ()
+printXmlSpecialData handle (x:xs) spaces symbol_ = case x of
+    Left _ -> printXmlSpecialData handle [x] spaces symbol_
+    Right _ -> return ()
+    >> printXmlSpecialData handle xs spaces symbol_
 printXmlObjectStartSymbol :: Maybe Handle -> Object -> Int -> IO Bool
 printXmlObjectStartSymbol handle
     obj@(Object {objType = SectionT, objSymbol = Just "section"}) spaces =
