ece9164a58dda3724e3814a66af068c438746e00
Thomaltarix <thomas.boue@epitech.eu>
2024-04-25 19:11:06 +0200

    rename: All the "print" functions to "get" functions

--- src/Json/JsonDisplay.hs
+++ src/Json/JsonDisplay.hs
@@ -32,9 +32,9 @@ getJsonDataTag data_ = case dataType data_ of
     BoldT -> ("\"", "\"")
     CodeT -> ("\"", "\"")
 
-printJsonSymbol ::String -> Int -> String
-printJsonSymbol "" spaces = getString "" spaces
-printJsonSymbol symbol_ spaces =
+getJsonSymbol ::String -> Int -> String
+getJsonSymbol "" spaces = getString "" spaces
+getJsonSymbol symbol_ spaces =
     getString "\"" spaces ++
     getString symbol_ 0 ++
     getString "\": " 0
@@ -42,50 +42,49 @@ printJsonSymbol symbol_ spaces =
 printJsonData :: Data -> Bool -> Int -> String
 printJsonData data_ end spaces =
     let (startTag, endTag) = getJsonDataTag data_ in
-    printJsonSymbol (myFromJustString (symbol data_)) spaces ++
+    getJsonSymbol (myFromJustString (symbol data_)) spaces ++
     getString startTag 0 ++
     getString (myFromJustString (dataContent data_)) 0 ++
     getString endTag 0 ++
     getEnd end
 
-printJsonContent :: [Either Data Object] -> Int -> String
-printJsonContent [] _ = ""
-printJsonContent [Left data_] spaces = printJsonData data_ True spaces
-printJsonContent [Right obj] spaces = printJsonObject obj True spaces
-printJsonContent (Left data_:xs) spaces = printJsonData data_ False spaces
-    ++ printJsonContent xs spaces
-printJsonContent (Right obj:xs) spaces = printJsonObject obj False spaces
-    ++ printJsonContent xs spaces
+getJsonContent :: [Either Data Object] -> Int -> String
+getJsonContent [] _ = ""
+getJsonContent [Left data_] spaces = printJsonData data_ True spaces
+getJsonContent [Right obj] spaces = getJsonObject obj True spaces
+getJsonContent (Left data_:xs) spaces = printJsonData data_ False spaces
+    ++ getJsonContent xs spaces
+getJsonContent (Right obj:xs) spaces = printJsonObject obj False spget    ++ getJsonContent xs spaces
 
-printJsonObject :: Object -> Bool -> Int -> String
-printJsonObject obj end spaces =
+getJsonObject :: Object -> Bool -> Int -> String
+getJsonObject obj end spaces =
     let (startTag, endTag) = getJsonObjectTag obj in
-    printJsonSymbol (myFromJustString (objSymbol obj)) spaces ++
+    getJsonSymbol (myFromJustString (objSymbol obj)) spaces ++
     getString startTag 0 ++
-    printJsonContent (datas obj) (spaces + 4) ++
+    getJsonContent (datas obj) (spaces + 4) ++
     getString endTag spaces ++
     getEnd end
 
-printJsonHeaderData :: Maybe Data -> Bool -> Int -> String
-printJsonHeaderData Nothing _ _ = ""
-printJsonHeaderData (Just data_) end spaces =
+getJsonHeaderData :: Maybe Data -> Bool -> Int -> String
+getJsonHeaderData Nothing _ _ = ""
+getJsonHeaderData (Just data_) end spaces =
     printJsonData data_ end spaces
 
-printJsonHeader :: Header -> Bool -> Int -> String
-printJsonHeader
+getJsonHeader :: Header -> Bool -> Int -> String
+getJsonHeader
     Header {title = title_, author = author_, date = date_} end spaces =
     let (startTag, endTag) = getJsonObjectTag (Object SectionT Nothing []) in
     getString "\"header\": " spaces ++
     getString startTag 0 ++
-    printJsonHeaderData title_ False (spaces + 4) ++
-    printJsonHeaderData author_ False (spaces + 4) ++
-    printJsonHeaderData date_ True (spaces + 4) ++
+    getJsonHeaderData title_ False (spaces + 4) ++
+    getJsonHeaderData author_ False (spaces + 4) ++
+    getJsonHeaderData date_ True (spaces + 4) ++
     endTag ++
     getEnd end
 
 printJson :: Maybe Handle -> DataStruct -> IO ()
 printJson handle dataStruct =
     let (startTag, endTag) = getJsonObjectTag (Object SectionT Nothing []) in
-    let json = startTag ++ printJsonHeader (header dataStruct) False 4
-            ++ printJsonObject (content dataStruct) True 4 ++ endTag ++ "\n" in
+    let json = startTag ++ getJsonHeader (header dataStruct) False 4
+            ++ getJsonObject (content dataStruct) True 4 ++ endTag ++ "\n" in
         printString handle json
--- src/Xml/XmlDisplay.hs
+++ src/Xml/XmlDisplay.hs
@@ -41,29 +41,29 @@ getXmlDataTag data_ = case dataType data_ of
     BoldT -> ("<bold>", "</bold>")
     CodeT -> ("<code>", "</code>")
 
-printXmlStartSymbol :: String -> Int -> String
-printXmlStartSymbol "" spaces = getString "" spaces
-printXmlStartSymbol symbol_ spaces =
+getXmlStartSymbol :: String -> Int -> String
+getXmlStartSymbol "" spaces = getString "" spaces
+getXmlStartSymbol symbol_ spaces =
     getString "<" spaces ++
     getString symbol_ 0 ++
     getString ">" 0
 
-printXmlEndSymbol :: String -> Int -> String
-printXmlEndSymbol "" spaces = getString "\n" spaces
-printXmlEndSymbol symbol_ _ =
+getXmlEndSymbol :: String -> Int -> String
+getXmlEndSymbol "" spaces = getString "\n" spaces
+getXmlEndSymbol symbol_ _ =
     getString "</" 0 ++
     getString symbol_ 0 ++
     getString ">\n" 0
 
-printXmlData :: Data -> Int -> String
-printXmlData Data {symbol = Just "title"} _ = ""
-printXmlData Data {symbol = Just "url"} _ = ""
-printXmlData data_@(Data {dataType = TextT}) spaces =
+getXmlData :: Data -> Int -> String
+getXmlData Data {symbol = Just "title"} _ = ""
+getXmlData Data {symbol = Just "url"} _ = ""
+getXmlData data_@(Data {dataType = TextT}) spaces =
     let (startTag, endTag) = getXmlDataTag data_ in
     getString startTag spaces ++
     getString (myFromJustString (dataContent data_)) 0 ++
     getString endTag 0
-printXmlData data_ _ =
+getXmlData data_ _ =
     let (startTag, endTag) = getXmlDataTag data_ in
     getString startTag 0 ++
     getString (myFromJustString (dataContent data_)) 0 ++
@@ -73,40 +73,40 @@ getEndOfSection :: Bool -> String
 getEndOfSection False = "\n"
 getEndOfSection True = ""
 
-printXmlSpecialData :: [Either Data Object] -> String -> Bool -> String
-printXmlSpecialData [Left data_] symbol_ end =
+getXmlSpecialData :: [Either Data Object] -> String -> Bool -> String
+getXmlSpecialData [Left data_] symbol_ end =
     getString " " 0 ++
     getString symbol_ 0 ++
     getString "=\"" 0 ++
     getString (myFromJustString (dataContent data_)) 0 ++
     getString "\">" 0 ++
     getEndOfSection end
-printXmlSpecialData (Left data_:xs) symbol_ end =
-    printXmlSpecialData [Left data_] symbol_ end ++
-    printXmlSpecialData xs symbol_ end
-printXmlSpecialData _ _ _ = ""
+getXmlSpecialData (Left data_:xs) symbol_ end =
+    getXmlSpecialData [Left data_] symbol_ end ++
+    getXmlSpecialData xs symbol_ end
+getXmlSpecialData _ _ _ = ""
 
-printXmlContent :: [Either Data Object] -> Int -> String
-printXmlContent [] _ = ""
-printXmlContent [Left data_] spaces = printXmlData data_ spaces
-printXmlContent [Right obj] spaces = printXmlObject obj spaces
-printXmlContent (Left data_:xs) spaces =
-    printXmlData data_ spaces ++
-    printXmlContent xs spaces
-printXmlContent (Right obj:xs) spaces = printXmlObject obj spaces
-    ++ printXmlContent xs spaces
+getXmlContent :: [Either Data Object] -> Int -> String
+getXmlContent [] _ = ""
+getXmlContent [Left data_] spaces = getXmlData data_ spaces
+getXmlContent [Right obj] spaces = getXmlObject obj spaces
+getXmlContent (Left data_:xs) spaces =
+    getXmlData data_ spaces ++
+    getXmlContent xs spaces
+getXmlContent (Right obj:xs) spaces = getXmlObject obj spaces
+    ++ getXmlContent xs spaces
 
 needUrl :: Object -> Int -> (String, XmlIndentation)
 needUrl obj spaces =
     let (startTag, _) = getXmlObjectTag obj in
     (getString startTag spaces
-    ++ printXmlSpecialData (datas obj) "url" True, NoSpaces)
+    ++ getXmlSpecialData (datas obj) "url" True, NoSpaces)
 
 needTitle :: Object -> Int -> (String, XmlIndentation)
 needTitle obj spaces =
     let (startTag, _) = getXmlObjectTag obj in
     (getString startTag spaces
-    ++ printXmlSpecialData (datas obj) "title" False, FourSpaces)
+    ++ getXmlSpecialData (datas obj) "title" False, FourSpaces)
 
 needNothing :: Object -> Int -> (String, XmlIndentation)
 needNothing obj spaces =
@@ -118,51 +118,51 @@ needNoSpaces obj spaces =
     let (startTag, _) = getXmlObjectTag obj in
     (getString startTag spaces, NoSpaces)
 
-printXmlObjectStartSymbol :: Object -> Int -> (String, XmlIndentation)
-printXmlObjectStartSymbol
+getXmlObjectStartSymbol :: Object -> Int -> (String, XmlIndentation)
+getXmlObjectStartSymbol
     obj@Object {objType = SectionT, objSymbol = Just "section"} spaces =
     needTitle obj spaces
-printXmlObjectStartSymbol
+getXmlObjectStartSymbol
     obj@Object {objType = ListT, objSymbol = Just "list"} spaces =
     needNothing obj spaces
-printXmlObjectStartSymbol
+getXmlObjectStartSymbol
     obj@Object {objType = CodeBlockT, objSymbol = Just "codeblock"} spaces =
     needNothing obj spaces
-printXmlObjectStartSymbol
+getXmlObjectStartSymbol
     obj@Object {objType = LinkT, objSymbol = Just "link"} spaces =
     needUrl obj spaces
-printXmlObjectStartSymbol
+getXmlObjectStartSymbol
     obj@Object {objType = ImageT, objSymbol = Just "image"} spaces =
     needUrl obj spaces
-printXmlObjectStartSymbol obj@Object {objType = ParagraphT} spaces =
+getXmlObjectStartSymbol obj@Object {objType = ParagraphT} spaces =
     needNoSpaces obj spaces
-printXmlObjectStartSymbol _ _ = ("", NormalSpaces)
+getXmlObjectStartSymbol _ _ = ("", NormalSpaces)
 
 displayEnd :: Object -> Int -> String
 displayEnd obj spaces =
     let (_, endTag) = getXmlObjectTag obj in
     getString endTag spaces
 
-printXmlObjectEndSymbol :: Object -> Int -> String
-printXmlObjectEndSymbol
+getXmlObjectEndSymbol :: Object -> Int -> String
+getXmlObjectEndSymbol
     obj@Object {objType = SectionT, objSymbol = Just "section"} spaces =
     displayEnd obj spaces
-printXmlObjectEndSymbol
+getXmlObjectEndSymbol
     obj@Object {objType = ListT, objSymbol = Just "list"} spaces =
     displayEnd obj spaces
-printXmlObjectEndSymbol
+getXmlObjectEndSymbol
     obj@Object {objType = CodeBlockT, objSymbol = Just "codeblock"} spaces =
     displayEnd obj spaces
-printXmlObjectEndSymbol
+getXmlObjectEndSymbol
     obj@Object {objType = LinkT, objSymbol = Just "link"} spaces =
     displayEnd obj spaces
-printXmlObjectEndSymbol
+getXmlObjectEndSymbol
     obj@Object {objType = ImageT, objSymbol = Just "image"} spaces =
     displayEnd obj spaces
-printXmlObjectEndSymbol
+getXmlObjectEndSymbol
     obj@Object {objType = ParagraphT} spaces =
     displayEnd obj spaces
-printXmlObjectEndSymbol _ _ = ""
+getXmlObjectEndSymbol _ _ = ""
 
 checkSubObject :: [Either Data Object] -> Bool
 checkSubObject [] = True
@@ -192,53 +192,53 @@ checkObject obj@(Object {objType = ListT})
 checkObject obj = obj
 
 handleSpaces :: Object -> Int -> XmlIndentation -> String
-handleSpaces obj spaces NormalSpaces = printXmlContent (datas obj) spaces
-    ++ printXmlObjectEndSymbol obj spaces
-handleSpaces obj spaces FourSpaces = printXmlContent (datas obj) (spaces + 4)
-    ++ printXmlObjectEndSymbol obj spaces
-handleSpaces obj _ NoSpaces = printXmlContent (datas obj) 0
-    ++ printXmlObjectEndSymbol obj 0
+handleSpaces obj spaces NormalSpaces = getXmlContent (datas obj) spaces
+    ++ getXmlObjectEndSymbol obj spaces
+handleSpaces obj spaces FourSpaces = getXmlContent (datas obj) (spaces + 4)
+    ++ getXmlObjectEndSymbol obj spaces
+handleSpaces obj _ NoSpaces = getXmlContent (datas obj) 0
+    ++ getXmlObjectEndSymbol obj 0
 
-printXmlObject :: Object -> Int -> String
-printXmlObject
+getXmlObject :: Object -> Int -> String
+getXmlObject
     obj@(Object {objType = ListT, objSymbol = Just "content"}) spaces =
-    let (str, indent) = printXmlObjectStartSymbol obj spaces in
+    let (str, indent) = getXmlObjectStartSymbol obj spaces in
     str ++ handleSpaces obj spaces indent
-printXmlObject obj spaces =
+getXmlObject obj spaces =
     let newObj = checkObject obj in
-    let (str, indent) = printXmlObjectStartSymbol newObj spaces in
+    let (str, indent) = getXmlObjectStartSymbol newObj spaces in
     str ++ handleSpaces newObj spaces indent
 
-printXmlHeaderData :: Maybe Data -> Int -> String
-printXmlHeaderData Nothing _ = ""
-printXmlHeaderData (Just data_@(Data {symbol = Just "title"})) _ =
+getXmlHeaderData :: Maybe Data -> Int -> String
+getXmlHeaderData Nothing _ = ""
+getXmlHeaderData (Just data_@(Data {symbol = Just "title"})) _ =
     getString (myFromJustString (dataContent data_)) 0
-printXmlHeaderData (Just data_) spaces =
-    printXmlStartSymbol (myFromJustString (symbol data_)) spaces
+getXmlHeaderData (Just data_) spaces =
+    getXmlStartSymbol (myFromJustString (symbol data_)) spaces
     ++ getString (myFromJustString (dataContent data_)) 0
-    ++ printXmlEndSymbol (myFromJustString (symbol data_)) 0
+    ++ getXmlEndSymbol (myFromJustString (symbol data_)) 0
 
-printXmlHeader :: Header -> Int -> String
-printXmlHeader header_ spaces =
+getXmlHeader :: Header -> Int -> String
+getXmlHeader header_ spaces =
     let (startTag, endTag) = getXmlTag HeaderT in
     getString startTag spaces
     ++ getString " title=\"" 0
-    ++ printXmlHeaderData (title header_) 0
+    ++ getXmlHeaderData (title header_) 0
     ++ getString "\">\n" 0
-    ++ printXmlHeaderData (author header_) (spaces + 4)
-    ++ printXmlHeaderData (date header_) (spaces + 4)
+    ++ getXmlHeaderData (author header_) (spaces + 4)
+    ++ getXmlHeaderData (date header_) (spaces + 4)
     ++ getString endTag spaces
 
-printXmlBody :: Object -> Int -> String
-printXmlBody obj spaces =
+getXmlBody :: Object -> Int -> String
+getXmlBody obj spaces =
     let (startTag, endTag) = getXmlTag BodyT in
     getString startTag spaces
-    ++ printXmlContent (datas obj) (spaces + 4)
+    ++ getXmlContent (datas obj) (spaces + 4)
     ++ getString endTag spaces
 
 printXml :: Maybe Handle -> DataStruct -> IO ()
 printXml handle dataStruct =
     let (startTag, endTag) = getXmlTag DocumentT in
-    let xml = startTag ++ printXmlHeader (header dataStruct) 4
-            ++ printXmlBody (content dataStruct) 4 ++ endTag ++ "\n" in
+    let xml = startTag ++ getXmlHeader (header dataStruct) 4
+            ++ getXmlBody (content dataStruct) 4 ++ endTag ++ "\n" in
         printString handle xml
