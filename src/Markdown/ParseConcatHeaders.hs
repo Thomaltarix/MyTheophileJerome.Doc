049b00e9a316853aa9c7b7e4a14a60bcb73804f7
Théophile JR <theophilejeromerocher44@gmail.com>
2024-04-26 10:11:20 +0200

    refactor: drastically reduce parseHeaderBd function markdown "

--- src/Markdown/ParseConcatHeaders.hs
+++ src/Markdown/ParseConcatHeaders.hs
@@ -22,26 +22,32 @@ concatList level =
     <|> parseHeaderBd level 1 "# " <|> parseCodeBlock 
     <|> parseListBlock  <|> parseParagraph ))
 
+wrapInEmptySection :: [Either Data Object] -> Object
+wrapInEmptySection o = (createObject SectionT Nothing [
+            Right (createObject SectionT (Just "section") [
+                (Left (createData (Just "") TextT (Just "title"))),
+                (Right (createObject ListT (Just "content") o))
+            ])])
+
+createHeaderSection :: String -> [Either Data Object] -> Object
+createHeaderSection title o = (createObject SectionT Nothing [
+            Right (createObject SectionT (Just "section") [
+                (Left (createData (Just title) TextT (Just "title"))),
+                (Right (createObject ListT (Just "content") o))
+            ])
+        ])
+
 parseHeaderBd :: Int -> Int -> String -> Parser (Either Data Object)
 parseHeaderBd clevel thislevel trigger 
     | ((thislevel - (clevel + 1)) > 0) = do
         a <- parseHeaderBd (clevel + 1) thislevel trigger
-        return (Right (createObject SectionT Nothing [
-            Right (createObject SectionT (Just "section") [
-                (Left (createData (Just "") TextT (Just "title"))),
-                (Right (createObject ListT (Just "content") [a]))
-            ])]))
+        return (Right (wrapInEmptySection [a]))
     | clevel < thislevel = do
     _ <- parseMany (parseAnyChar "\n \t")
     _ <- parseString trigger
     title <- parseUntilChar '\n'
     content <- concatList thislevel
-    return (Right (createObject SectionT Nothing [
-            Right (createObject SectionT (Just "section") [
-                (Left (createData (Just title) TextT (Just "title"))),
-                (Right (createObject ListT (Just "content") content))
-            ])
-        ]))
+    return (Right (createHeaderSection title content))
     | otherwise = do
         _ <- failingParse
         return (Left (createData (Just "Error") TextT Nothing))
