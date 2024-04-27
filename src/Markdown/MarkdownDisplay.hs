8ec7c96eef6efcd0da1d1b5fac560f958f75d52e
Kylian TRANCHET <kylian.tranchet@epitech.eu>
2024-04-27 14:16:10 +0200

    feat: prasing of content of md (wip nb of '#' for sub sections)

--- src/Markdown/MarkdownDisplay.hs
+++ src/Markdown/MarkdownDisplay.hs
@@ -16,6 +16,8 @@ import DataStructure
 import PrintString
 import Control.Monad
 import Control.Exception (handle)
+import Data.Coerce (coerce)
+import Json.ParseContent (getContent)
 
 getMarkdownObjectTag :: Object -> (String, String)
 getMarkdownObjectTag obj = case objType obj of
@@ -63,47 +65,136 @@ printMarkdownHeader handle
     printMarkdownHeaderData handle date_ True spaces >>
     printString handle "---\n\n" spaces
 
-printMarkdownData :: Maybe Handle -> Data -> Int -> IO ()
-printMarkdownData handle data_ spaces =
+printListSymbol :: Maybe Handle -> Data -> Bool -> IO ()
+printListSymbol handle data_ list = when list $ printString handle "- " 0
+
+printMarkdownData :: Maybe Handle -> Data -> Bool -> IO ()
+printMarkdownData handle data_ list =
     let (startTag, endTag) = getMarkdownDataTag data_ in
     printString handle startTag 0 >>
+    printListSymbol handle data_ list >>
     printString handle (myFromJustString (dataContent data_)) 0 >>
     when (symbol data_ == Just "title") (printString handle "\n\n" 0) >>
     printString handle endTag 0
 
-printMarkdownContent :: Maybe Handle -> [Either Data Object] -> Int -> IO ()
-printMarkdownContent _ [] _ = return ()
-printMarkdownContent handle [x] spaces = case x of
-    Left data_ -> printMarkdownData handle data_ spaces
-    Right obj -> printMarkdownObject handle obj spaces
-    >> printMarkdownContent handle [] spaces
-printMarkdownContent handle (x:xs) spaces = case x of
-    Left data_ -> printMarkdownData handle data_ spaces
-    Right obj -> printMarkdownObject handle obj spaces
-    >> printMarkdownContent handle xs spaces
+printMarkdownContent :: Maybe Handle -> [Either Data Object] -> Int -> Bool -> IO ()
+printMarkdownContent _ [] _ _ = return ()
+-- printMarkdownContent handle [x] doubleReturn list = case x of
+--     Left data_ -> printMarkdownData handle data_ list
+--     Right obj -> printMarkdownObject handle obj doubleReturn list
+--     >> printMarkdownContent handle [] doubleReturn list
+printMarkdownContent handle_ (x:xs) nbReturn list = case x of
+    Left data_ -> printMarkdownData handle_ data_ list
+    Right obj -> printMarkdownObject handle_ obj nbReturn list
+    >> printMarkdownContent handle_ xs nbReturn list
 
-printEndSection :: Maybe Handle -> Object -> String -> IO ()
-printEndSection handle obj endTag = let data_ = datas obj in
-    case [data_] of
-        [[Left (Data _ _ (Just "bold"))]] -> printString handle endTag 0
-        [[Left (Data _ _ (Just "italic"))]] -> printString handle endTag 0
-        [[Left (Data _ _ (Just "code"))]] -> printString handle endTag 0
-        _ -> printString handle (endTag ++ "\n\n") 0
+displayReturns :: Maybe Handle -> Int -> IO ()
+displayReturns _ 0 = return ()
+displayReturns handle_ nb = printString handle_ "\n" 0 >> displayReturns handle_ (nb - 1)
 
-printMarkdownObject :: Maybe Handle -> Object -> Int -> IO ()
-printMarkdownObject handle obj spaces =
+printEndSection :: Maybe Handle -> Object -> String -> Int -> IO ()
+printEndSection handle_ obj endTag nbReturn = let data_ = datas obj in
+   case [data_] of
+       [[Left (Data _ _ (Just "bold"))]] -> printString handle_ endTag 0
+       [[Left (Data _ _ (Just "italic"))]] -> printString handle_ endTag 0
+       [[Left (Data _ _ (Just "code"))]] -> printString handle_ endTag 0
+       _ -> displayReturns handle_ nbReturn >> printString handle_ endTag 0
+
+calcNbReturnHelper :: [Either Data Object] -> Int
+calcNbReturnHelper [] = 2
+calcNbReturnHelper (Left _:x) = calcNbReturnHelper x
+calcNbReturnHelper (Right xs:_) = case objSymbol xs of
+    Just "link" -> 0
+    Just "image" -> 0
+    _ -> 2
+
+calcNbReturn :: Int -> Object -> Int
+calcNbReturn 1 _ = 0
+calcNbReturn 0 _ = 1
+calcNbReturn _ obj = case objType obj of
+    CodeBlockT -> 1
+    _ -> case objSymbol obj of
+        Just "list" -> 0
+        _ -> calcNbReturnHelper (datas obj)
+
+isList :: Bool -> Object -> Bool
+isList True _ = True
+isList False obj = case objType obj of
+    ListT -> case objSymbol obj of
+        Just "list" -> True
+        _ -> False
+    _ -> False
+
+getUrl :: [Either Data Object] -> String
+getUrl [] = ""
+getUrl (Left data_:x) = if symbol data_ == Just "url"
+                                then myFromJustString (dataContent data_)
+                                else getUrl x
+getUrl (Right _:x) = getUrl x
+
+getContentUrl :: [Either Data Object] -> String
+getContentUrl [] = ""
+getContentUrl (Left data_:x) = if symbol data_ == Nothing
+                                then myFromJustString (dataContent data_)
+                                else getContentUrl x
+getContentUrl (Right xs:x) = if objSymbol xs == Just "content"
+                                then getContentUrl (datas xs)
+                                else getContentUrl x
+
+printMarkdownLink :: Maybe Handle -> Object -> IO ()
+printMarkdownLink handle_ obj =
+    let url = getUrl (datas obj)
+        content_ = getContentUrl (datas obj) in
+    printString handle_ "[" 0 >>
+    printString handle_ content_ 0 >>
+    printString handle_ "](" 0 >>
+    printString handle_ url 0 >>
+    printString handle_ ")" 0
+
+getContentImage :: [Either Data Object] -> String
+getContentImage [] = ""
+getContentImage (Left data_:x) = if symbol data_ == Nothing
+                                then myFromJustString (dataContent data_)
+                                else getContentImage x
+getContentImage (Right xs:x) = if objSymbol xs == Just "alt"
+                                then getContentImage (datas xs)
+                                else getContentImage x
+
+printMarkdownImage :: Maybe Handle -> Object -> IO ()
+printMarkdownImage handle_ obj =
+    let url = getUrl (datas obj)
+        content_ = getContentImage (datas obj) in
+    printString handle_ "![" 0 >>
+    printString handle_ content_ 0 >>
+    printString handle_ "](" 0 >>
+    printString handle_ url 0 >>
+    printString handle_ ")" 0
+
+needPrintStartTag :: [Either Data Object] -> Bool
+needPrintStartTag [] = False
+needPrintStartTag (Left data_:_) = symbol data_ == Just "title" && dataContent data_ /= Just ""
+needPrintStartTag (Right _:_) = False
+
+printStartTag :: Maybe Handle -> Object -> Int -> IO ()
+printStartTag handle_ obj@(Object {objType = CodeBlockT}) _ = printString handle_ "```\n" 0
+printStartTag handle_ obj spaces = when (needPrintStartTag (datas obj)) $ printString handle_ "# " spaces
+
+printMarkdownObject :: Maybe Handle -> Object -> Int -> Bool -> IO ()
+printMarkdownObject handle obj nbReturn list =
     let (startTag, endTag) = getMarkdownObjectTag obj in
     case objSymbol obj of
-        Nothing ->  printMarkdownContent handle (datas obj) 0 >>
-                    printEndSection handle obj endTag
-        Just _ ->   printString handle startTag 0 >>
-                    printMarkdownContent handle (datas obj) 0 >>
-                    printEndSection handle obj endTag
+        Nothing ->  printMarkdownContent handle (datas obj) (calcNbReturn nbReturn obj) (isList list obj) >>
+                    printEndSection handle obj endTag (calcNbReturn nbReturn obj)
+        Just "link" -> printMarkdownLink handle obj
+        Just "image" -> printMarkdownImage handle obj
+        Just _ ->   printStartTag handle obj 0 >>
+                    printMarkdownContent handle (datas obj) (calcNbReturn nbReturn obj) (isList list obj) >>
+                    printEndSection handle obj endTag (calcNbReturn nbReturn obj)
 
 printMarkdown :: Maybe Handle -> DataStruct -> IO ()
 printMarkdown handle dataStruct =
     let (_, endTag) = getMarkdownObjectTag (Object SectionT Nothing []) in
-    printMarkdownHeader handle (header dataStruct) False 0 >>
-    printMarkdownObject handle (content dataStruct) 0 >>
+    printMarkdownHeader handle (header dataStruct) True 0 >>
+    printMarkdownObject handle (content dataStruct) 2 False >>
     printString handle endTag 0 >>
     printString handle "\n" 0
